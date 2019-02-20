09ef0c50651b7f958d6a09513f7481d7e40c7963
amanriquez <amanriquez.guitar@gmail.com>
2019-02-19 23:24:35 -0600

    finished REST

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require("express");
-bodyParser = require("body-parser");
-mongoose = require("mongoose");
-app = express();
+const express = require("express");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
 
 // APP CONFIG
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true }
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
  
 
 //MONGOOSE MODEL CONFIG
@@ -52,13 +54,72 @@ app.post("/blogs", function(req, res){
     //create blog
     Blog.create(req.body.blog, function(err, newBlog){
         if (err){
-            res.render("new")
+            res.render("new");
         } else{
             res.redirect("/blogs");
         }
     })
     //redirect to index
 })
+
+
+//SHOW ROUTE
+
+app.get("/blogs/:id", function(req, res){
+
+Blog.findById(req.params.id, function(err, foundBlog){
+    if (err){
+        res.redirect("/blogs");
+    } else{
+        res.render("show", {blog: foundBlog});
+    }
+})
+})
+
+//EDIT
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    })
+        
+})
+
+
+//UPDATE
+
+app.put("/blogs/:id", function(req, res){
+
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+
+})
+
+//DELETE ROUTE
+
+app.delete("/blogs/:id", function(req, res){
+
+//destroy
+Blog.findByIdAndRemove(req.params.id, function(err){
+    if (err){
+        res.redirect("/blogs");
+    } else{
+        res.redirect("/blogs");
+    }
+})
+//redirect
+
+})
+
 
 
 //LISTEN
